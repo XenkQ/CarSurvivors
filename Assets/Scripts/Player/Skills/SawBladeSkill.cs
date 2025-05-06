2026f1138d8fd369676a884ecc29463bc7818cfe
Konrad Blaszyński <blaszynski.konrad@gmail.com>
2025-05-06 21:02:25 +0200

    CS-7 - bouncing when in contact with saw skill

--- Assets/Scripts/Player/Skills/SawBladeSkill.cs
+++ Assets/Scripts/Player/Skills/SawBladeSkill.cs
@@ -1,9 +1,12 @@
 using System;
+using DG.Tweening;
 using LayerMasks;
 using UnityEngine;
 
 public class SawBladeSkill : Skill
 {
+    [SerializeField] private float _knockbackPower = 2f;
+    private float _knockbackMoveSpeed = 0.4f;
     [SerializeField] private float _damage;
     [SerializeField] private float _size;
     [SerializeField] private float _cooldown;
@@ -11,6 +14,8 @@ public class SawBladeSkill : Skill
     private int _level;
 
     [SerializeField] private BoxCollider _boxCollider;
+
+    public override event EventHandler OnLevelUp;
 
     private void Awake()
     {
@@ -22,13 +27,6 @@ public class SawBladeSkill : Skill
         _currentCooldown = _cooldown;
     }
 
-    public override event EventHandler OnLevelUp;
-
-    public override void LevelUp()
-    {
-        _level++;
-    }
-
     public void Update()
     {
         if (_currentCooldown > 0)
@@ -37,20 +35,41 @@ public class SawBladeSkill : Skill
         }
         else
         {
-            RaycastHit[] colliders = Physics.BoxCastAll(transform.position + _boxCollider.center, _boxCollider.size, Vector3.down, transform.rotation);
-            foreach (RaycastHit collider in colliders)
-            {
-                GameObject collisionObject = collider.transform.gameObject;
-                Debug.Log(collisionObject.name);
-                if ((1 << collisionObject.layer) == EntityLayers.Enemy)
-                {
-                    if (collisionObject.TryGetComponent(out IDamageable damageable))
-                    {
-                        damageable.TakeDamage(_damage);
-                    }
-                }
-            }
+            AtackAllEnemiesInsideCollider();
             _currentCooldown = _cooldown;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        AttackCollidingEntity(other);
+    }
+
+    public override void LevelUp()
+    {
+        _level++;
+    }
+
+    private void AtackAllEnemiesInsideCollider()
+    {
+        RaycastHit[] hits = Physics.BoxCastAll(transform.position + _boxCollider.center, _boxCollider.size, Vector3.down, transform.rotation);
+        foreach (RaycastHit hit in hits)
+        {
+            AttackCollidingEntity(hit.collider);
+        }
+    }
+
+    private void AttackCollidingEntity(Collider other)
+    {
+        GameObject collisionObject = other.transform.gameObject;
+        Debug.Log(collisionObject.name);
+        if ((1 << collisionObject.layer) == EntityLayers.Enemy)
+        {
+            if (collisionObject.TryGetComponent(out IDamageable damageable))
+            {
+                collisionObject.transform.DOMove(other.transform.position + transform.forward * _knockbackPower, _knockbackMoveSpeed);
+                damageable.TakeDamage(_damage);
+            }
         }
     }
 }
