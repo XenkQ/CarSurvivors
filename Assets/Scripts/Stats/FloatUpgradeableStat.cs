30d9daca252d51054016712f885e94cc11094b48
Konrad Blaszyński <blaszynski.konrad@gmail.com>
2025-07-04 12:45:49 +0200

    CS-7 - upgradeable stats after serialization update

---
+++ Assets/Scripts/Stats/FloatUpgradeableStat.cs
@@ -0,0 +1,36 @@
+﻿using Assets.Scripts.CustomTypes;
+using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Stats
+{
+    // For unity serialization we need to use nongeneric class.
+
+    [Serializable]
+    public class FloatUpgradeableStat : UpgradeableStat<float>
+    {
+        [SerializeField] private FloatValueRange _floatMinMaxRange;
+        [SerializeField] private FloatValueRange _floatRangeOfPossibleValuesForUpgrade;
+
+        public FloatUpgradeableStat(float value, float maxValue, FloatValueRange minMaxRange, FloatValueRange rangeOfPossibleValuesForUpgrade, bool alwaysUseMinValueForUpgrade = false)
+            : base(value, minMaxRange, rangeOfPossibleValuesForUpgrade, alwaysUseMinValueForUpgrade)
+        {
+        }
+
+        public FloatUpgradeableStat(float value, float maxValue, bool alwaysUseMinValueForUpgrade = false)
+            : base(value, alwaysUseMinValueForUpgrade)
+        {
+            MinMaxRange = _floatMinMaxRange;
+            _rangeOfPossibleValuesForUpgrade = _floatRangeOfPossibleValuesForUpgrade;
+        }
+
+        public override void OnAfterDeserialize()
+        {
+            base.OnAfterDeserialize();
+
+            MinMaxRange = _floatMinMaxRange;
+            _rangeOfPossibleValuesForUpgrade = _floatRangeOfPossibleValuesForUpgrade;
+            CanBeUpgraded = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Stats/UpgradeableStat.cs
@@ -0,0 +1,145 @@
+﻿using Assets.Scripts.CustomTypes;
+using Assets.Scripts.Skills;
+using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Stats
+{
+    public interface IUpgradeableStat
+    {
+        public bool CanBeUpgraded { get; }
+
+        public bool IsSubstractModeOn { get; }
+
+        public StatsUnits Unit { get; }
+
+        public void Upgrade(float upgradeValue);
+
+        public float GetUpgradeValueBasedOnUpdateRange();
+
+        public float GetWhatPercentOfValueIsUpgradeValue(float upgradeValue);
+
+        public event EventHandler OnUpgrade;
+    }
+
+    [Serializable]
+    public abstract class UpgradeableStat<T> : IUpgradeableStat, ISerializationCallbackReceiver
+        where T : struct, IComparable<T>, IConvertible
+    {
+        [field: SerializeField] public bool IsSubstractModeOn { get; protected set; }
+        [field: SerializeField] public StatsUnits Unit { get; protected set; }
+        [field: SerializeField] public T Value { get; protected set; }
+        [SerializeField] protected bool _alwaysUseMinValueForUpgrade;
+        public bool CanBeUpgraded { get; protected set; } = true;
+        public ValueRange<T> MinMaxRange { get; protected set; }
+        protected ValueRange<T> _rangeOfPossibleValuesForUpgrade;
+
+        public event EventHandler OnUpgrade;
+
+        public UpgradeableStat(
+            T value,
+            bool alwaysUseMinValueForUpgrade = false)
+        {
+            Value = value;
+            _alwaysUseMinValueForUpgrade = alwaysUseMinValueForUpgrade;
+        }
+
+        public UpgradeableStat(
+            T value,
+            ValueRange<T> minMaxRange,
+            ValueRange<T> rangeOfPossibleValuesForUpgrade,
+            bool alwaysUseMinValueForUpgrade = false) : this(value, alwaysUseMinValueForUpgrade)
+        {
+            MinMaxRange = minMaxRange;
+            _rangeOfPossibleValuesForUpgrade = rangeOfPossibleValuesForUpgrade;
+        }
+
+        public virtual void Upgrade(float upgradeValue)
+        {
+            if (!CanBeUpgraded)
+            {
+                return;
+            }
+
+            float value = Convert.ToSingle(Value);
+            float minValue = Convert.ToSingle(MinMaxRange.Min);
+            float maxValue = Convert.ToSingle(MinMaxRange.Max);
+
+            float delta = _alwaysUseMinValueForUpgrade ? minValue : upgradeValue;
+            delta = IsSubstractModeOn ? -delta : delta;
+
+            float newValue = value + delta;
+
+            if (IsValueExceedingOrEqualMaxValue(newValue, maxValue))
+            {
+                newValue = maxValue;
+                CanBeUpgraded = false;
+            }
+
+            Value = FromFloatToType(newValue);
+
+            OnUpgrade?.Invoke(this, EventArgs.Empty);
+        }
+
+        public float GetUpgradeValueBasedOnUpdateRange()
+        {
+            return Convert.ToSingle(_rangeOfPossibleValuesForUpgrade.GetRandomValueInRange());
+        }
+
+        public float GetWhatPercentOfValueIsUpgradeValue(float upgradeValue)
+        {
+            return Convert.ToSingle(upgradeValue) / Convert.ToSingle(Value) * 100f;
+        }
+
+        public virtual void OnBeforeSerialize()
+        {
+        }
+
+        public virtual void OnAfterDeserialize()
+        {
+            if (IsValueBelowMinValue(Value, MinMaxRange.Min))
+            {
+                Value = MinMaxRange.Min;
+            }
+
+            if (IsValueExceedingOrEqualMaxValue(Value, MinMaxRange.Max))
+            {
+                Value = MinMaxRange.Max;
+                CanBeUpgraded = false;
+            }
+        }
+
+        private bool IsValueExceedingOrEqualMaxValue(T value, T maxValue)
+        {
+            float valueFloat = Convert.ToSingle(value);
+            float maxValueFloat = Convert.ToSingle(maxValue);
+
+            return IsValueExceedingOrEqualMaxValue(valueFloat, maxValueFloat);
+        }
+
+        private bool IsValueExceedingOrEqualMaxValue(float value, float maxValue)
+        {
+            return IsSubstractModeOn && value <= maxValue
+                || !IsSubstractModeOn && value >= maxValue;
+        }
+
+        private bool IsValueBelowMinValue(T value, T minValue)
+        {
+            float valueFloat = Convert.ToSingle(value);
+            float minValueFloat = Convert.ToSingle(minValue);
+
+            return IsValueBelowMinValue(valueFloat, minValueFloat);
+        }
+
+        private bool IsValueBelowMinValue(float value, float minValue)
+        {
+            return IsSubstractModeOn && value >= minValue
+                || !IsSubstractModeOn && value <= minValue;
+        }
+
+        private T FromFloatToType(float value)
+        {
+            return (T)Convert.ChangeType(value, typeof(T));
+        }
+    }
+}
