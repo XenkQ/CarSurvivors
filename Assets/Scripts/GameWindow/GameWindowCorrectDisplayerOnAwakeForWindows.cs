9e4f75fd0d2f54c6326ad03606443fcac63989f0
Konrad Blaszyński <blaszynski.konrad@gmail.com>
2025-08-13 18:08:43 +0200

    CS-33 - fixing bug with window incorrect display on multiple monitors setup on window

---
+++ Assets/Scripts/GameWindow/GameWindowCorrectDisplayerOnAwakeForWindows.cs
@@ -0,0 +1,61 @@
+﻿/*
+ *
+ * AI-Generated Code
+ *
+ */
+
+using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+
+public class GameWindowCorrectDisplayerOnAwakeForWindows : MonoBehaviour
+{
+#if UNITY_STANDALONE_WIN && !UNITY_EDITOR
+    [DllImport("user32.dll")]
+    private static extern bool GetWindowRect(IntPtr hWnd, out RECT rect);
+
+    [DllImport("user32.dll")]
+    private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter,
+        int X, int Y, int cx, int cy, uint uFlags);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow();
+
+    private const uint SWP_NOZORDER = 0x0004;
+    private const uint SWP_NOACTIVATE = 0x0010;
+
+    private struct RECT
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+    }
+
+    private void Awake()
+    {
+        CenterOnPrimaryMonitor();
+    }
+
+    private void CenterOnPrimaryMonitor()
+    {
+        IntPtr hWnd = GetActiveWindow();
+        if (hWnd == IntPtr.Zero)
+            return;
+
+        RECT rect;
+        GetWindowRect(hWnd, out rect);
+        int windowWidth = rect.Right - rect.Left;
+        int windowHeight = rect.Bottom - rect.Top;
+
+        int screenWidth = Display.displays[0].systemWidth;
+        int screenHeight = Display.displays[0].systemHeight;
+
+        int x = (screenWidth - windowWidth) / 2;
+        int y = (screenHeight - windowHeight) / 2;
+
+        SetWindowPos(hWnd, IntPtr.Zero, x, y, windowWidth, windowHeight, SWP_NOZORDER | SWP_NOACTIVATE);
+    }
+
+#endif
+}
