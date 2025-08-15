0d0f2b41d5b6524315b364e5c9e1ecad33b0516b
Konrad Blaszyński <blaszynski.konrad@gmail.com>
2025-08-15 11:29:44 +0200

    CS-33 - fixing bug with panels not disabled when switching

--- Assets/Scripts/UI/MenuButtonsFunctionality.cs
+++ Assets/Scripts/UI/MenuButtonsFunctionality.cs
@@ -8,6 +8,8 @@ namespace Assets.Scripts.UI
     {
         [Inject] private readonly IGameSceneLoader _gameSceneLoader;
 
+        [SerializeField] private GameObject[] _enabledDisabledPanels;
+
         public void OnSceneLoadButtonClick(string scene)
         {
             if (Enum.TryParse(scene, true, out GameScene gameScene))
@@ -18,6 +20,19 @@ namespace Assets.Scripts.UI
             {
                 Debug.LogError($"Invalid scene name: {scene}");
             }
+        }
+
+        public void EnableSingleObjectDisableOthers(GameObject gameObject)
+        {
+            foreach (var panel in _enabledDisabledPanels)
+            {
+                if (panel != gameObject)
+                {
+                    panel.SetActive(false);
+                }
+            }
+
+            gameObject.SetActive(true);
         }
 
         public void ToogleActivityOfObject(GameObject gameObject)
